aa32e537f4371698f0289b33445491d4baaba040
Sebastian Garces <segarces23@gmail.com>
2020-04-17 11:29:58 -0400

    Base Page for Products
    Base and Home page to display the products. Added navigation bar

--- PDimension/settings.py
+++ PDimension/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
