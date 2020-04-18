7903c2f0e783346e5a34701704d81a283537a624
Sebastian Garces <segarces23@gmail.com>
2020-04-18 09:48:20 -0400

    Creation of the Orders Module

--- PDimension/settings.py
+++ PDimension/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders.apps.OrdersConfig',
     'products.apps.ProductsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
