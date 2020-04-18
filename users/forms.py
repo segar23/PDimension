9f62dc5e428fc2ff28bd45ac9b091bc334c7a874
Sebastian Garces <segarces23@gmail.com>
2020-04-18 09:49:40 -0400

    Registration Form set to English

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(label='Correo Electrónico', required=True,)
-    username = forms.CharField(label='Nombre de Usuario', max_length=150,
-                               help_text='Requerido. 150 caracteres o menos. Letras o dígitos solamente.')
-    first_name = forms.CharField(label='Nombre', max_length=100, required=True)
-    last_name = forms.CharField(label='Apellido', max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
