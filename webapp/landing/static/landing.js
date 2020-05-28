d3d32b6ed0b0d21578000dae3a44d564a32773e1
Sebastian Garces <segarces23@gmail.com>
2020-05-28 11:06:45 -0400

    Fixing naming issue with JavaScript

---
+++ webapp/landing/static/landing.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("Document is ready");
+
+
+  /*$( ".card" ).hover(
+      function() {
+        $(this).addClass('shadow-lg').css('cursor', 'pointer');
+      }, function() {
+        $(this).removeClass('shadow-lg');
+      }
+  );
+
+  $("#phoneCard").hover(
+      function() {
+        $(this).addClass('btn btn-primary').css('cursor', 'pointer');
+      }, function() {
+        $(this).removeClass('btn btn-primary');
+      }
+  );*/
+
+// document ready
+});
