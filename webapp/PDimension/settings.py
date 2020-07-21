a8d0dd4f832e100ebeb1a59b9ef9801add54a699
Sebastian Garces <segarces23@gmail.com>
2020-07-21 08:59:35 -0400

    Fix in Python PEP

--- webapp/PDimension/settings.py
+++ webapp/PDimension/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dt7@zu&_j6#$p9z_kjknu8)jot6&5%q9#mn1!3dm7243iq*!k1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['papeleriadimension.com','www.papeleriadimension.com']
+ALLOWED_HOSTS = ['papeleriadimension.com', 'www.papeleriadimension.com']
 
 
 # Application definition
