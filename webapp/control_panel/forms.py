b92bbba1efe2d9edf93fa67b89cac570efc67889
Sebastian Garces <segarces23@gmail.com>
2020-07-21 08:58:05 -0400

    Fix in Category Form

--- webapp/control_panel/forms.py
+++ webapp/control_panel/forms.py
@@ -7,6 +7,10 @@ class CategoryCreateForm (ModelForm):
     name = forms.CharField(label='Nombre', required=True, error_messages={'unique': 'Esta categoría ya existe'})
     isMacro = forms.BooleanField(label='Es Macro-Categoría?', required=False)
 
+    def clean_name(self):
+        name = self.cleaned_data['name'].title()
+        return name
+
     class Meta:
         model = Category
         fields = ['name', 'isMacro']
