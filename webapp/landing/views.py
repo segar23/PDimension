23ada3fab9907dc670fb1582100edd21bcdc0d7d
Sebastian Garces <segarces23@gmail.com>
2020-08-07 10:10:04 -0400

    Working Search Bar
    Search Name and Description

--- webapp/landing/views.py
+++ webapp/landing/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView
+from functools import reduce
+from operator import or_ as OR
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView
+from control_panel.models import Product
 
 
 class LandingHome (TemplateView):
@@ -13,9 +17,17 @@ class LocationView (TemplateView):
     template_name = 'landing/location.html'
 
 
-class CatalogView (TemplateView):
+class CatalogView (ListView):
     template_name = 'landing/catalog.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 15
 
-
-class ProductsView (TemplateView):
-    template_name = 'landing/products-temp.html'
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        complete_set = Product.objects.all()
+        if query is None or query == '':
+            return complete_set
+        else:
+            query = reduce(OR, (Q(name__icontains=item) | Q(description__icontains=item) for item in query.split()))
+            return Product.objects.filter(query)
