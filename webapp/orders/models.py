c6f07501ac3cfb389cbb2bef7f00bb2b6b6a48f3
Sebastian Garces <segarces23@gmail.com>
2020-09-02 11:11:12 -0500

    Shopping Cart Visual Base

--- webapp/orders/models.py
+++ webapp/orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from control_panel.models import Product
+
 
 # Create your models here.
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def get_sub_total(self):
+        return self.product.price * self.quantity
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(OrderItem)
+
+    def get_total(self):
+        total = 0
+        for oder_item in self.products.all():
+            total += oder_item.get_sub_total()
+        return total
+
+    def clear_cart(self):
+        return self.products.clear()
+
+
